eb155858cac336057444a584f6ce49b31610b8aa
thaerachman <71365565+thaerachman@users.noreply.github.com>
2020-10-14 15:35:26 +0700

    Add files via upload

---
+++ Tugas_day2_Film.sql
@@ -0,0 +1,18 @@
+CREATE TABLE tugas_day2_Film
+(
+nomor INT,
+judul ENUM ('Avatar','Titanic','Star Wars :The Force Awakens','Jurassic World','The Avengers'),
+pendapatan BIGINT,
+tahun YEAR
+);
+
+SHOW COLUMNS FROM tugas_day2_Film;
+
+INSERT INTO tugas_day2_Film VALUES
+(1,'Avatar',2787965087,'2009'),
+(2,'Titanic',2186772302,'1997'),
+(3,'Star Wars :The Force Awakens',2068223624,'2015'),
+(4,'Jurassic World',1671713208,'2015'),
+(5,'The Avengers',1518812988,'2012');
+
+SELECT * FROM tugas_day2_film;
